d4ce925c02ea32bb96783def39264e2ac2b6e218
Abhishek Josyula <91714711+DefenseIsAlie@users.noreply.github.com>
2022-11-06 12:04:00 +0530

    daily challenge

---
+++ LeetCode/6_11_22/Solution.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String test = "";
+        int k = 0;
+        System.out.println(s.orderlyQueue(test, k));
+        System.out.println(); 
+    }
+
+    public String orderlyQueue(String s, int k) {
+
+        String ret = s;
+        
+        if (k==1){
+
+            for (int i = 0; i < s.length(); i++) {
+                String temp = s.substring(i) + s.substring(0, i);
+                if (temp.compareTo(ret)<0){
+                    ret = temp;
+                }
+            }
+
+            return ret;
+        }
+
+        char[] temp = s.toCharArray();
+        Arrays.sort(temp);
+        ret = new String(temp);
+
+        return ret;
+    }
+}
