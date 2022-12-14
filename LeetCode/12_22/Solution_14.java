72fe0b6622c4c8a563342c24ffe094a6d550a768
Abhishek Josyula <91714711+DefenseIsAlie@users.noreply.github.com>
2022-12-14 14:09:31 +0530

    Completed 14/12/22 leetcode

---
+++ LeetCode/12_22/Solution_14.java
@@ -0,0 +1,9 @@
+public class Solution_14{
+
+    public int rob(int[] nums) {
+        int prev=0, last=0;
+     for (int curr : nums)
+         last = Math.max(prev+curr, prev=last);
+     return last;
+    }
+}
